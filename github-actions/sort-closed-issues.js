d9b1c82666c3766d12f95ef75bc77b02aa36ec69
blakes24 <blakes24@gmail.com>
2021-09-11 15:18:23 -0700

    remove global variable

--- github-actions/sort-closed-issues.js
+++ github-actions/sort-closed-issues.js
@@ -1,14 +1,9 @@
-// Global variables
-let context;
-
 /**
  * Check the labels of an issue, and return the column the issue should be sorted into when closed
- * @param {Object} c - context object
+ * @param {Object} context - context object
  * @returns - returns an object with the action's result, which is passed on to the next action
  */
-function main({ c }) {
-  context = c;
-
+function main({ context }) {
   /** if issue includes these labels there should be no visual changes and it should be moved to the Done column */
   const doneLabels = [
     "role: back end/devOps",
@@ -19,7 +14,7 @@ function main({ c }) {
     "P-Feature: Home page",
   ];
 
-  const labels = obtainLabels();
+  const labels = obtainLabels(context);
 
   for (let label of labels) {
     if (doneLabels.includes(label)) {
@@ -34,7 +29,7 @@ function main({ c }) {
  * Get all labels from the issue
  * @return {Array} - returns an array of all the labels
  */
-function obtainLabels() {
+function obtainLabels(context) {
   const labelsObject = context.payload.issue.labels;
   const labels = labelsObject.map((label) => label.name);
   return labels;
