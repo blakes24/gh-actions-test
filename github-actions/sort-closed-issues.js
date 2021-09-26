a20cc99d8a0e43b0fc559dd2797af1978684dd27
blakes24 <blakes24@gmail.com>
2021-09-26 08:49:26 -0700

    go back to arrow functions

--- github-actions/sort-closed-issues.js
+++ github-actions/sort-closed-issues.js
@@ -5,54 +5,47 @@ const obtainLabels = require("./obtain-labels");
  * @param {Object} context - context object from actions/github-script
  * @returns - returns the appropriate column, which is passed on to the next action
  */
-function main({ context }) {
-  const doneColumn = "Done";
-  const UATColumn = "UAT";
+ function main({ context }) {
+   const doneColumn = "Done";
+   const UATColumn = "UAT";
 
-  const hardLabels = [
-    "Feature: Refactor CSS",
-    "Feature: Refactor HTML",
-    "Feature: Refactor JS / Liquid",
-    "Feature: Refactor GHA",
-  ];
+   const hardLabels = [
+     "Feature: Refactor CSS",
+     "Feature: Refactor HTML",
+     "Feature: Refactor JS / Liquid",
+     "Feature: Refactor GHA",
+   ];
 
-  const softLabels = ["role: back end/devOps", "Feature: Analytics"];
+   const softLabels = ["role: back end/devOps", "Feature: Analytics"];
 
-  const overrideSoftLabels = ["role: front end"];
+   const overrideSoftLabels = ["role: front end"];
 
-  const issueLabels = obtainLabels(context);
+   const issueLabels = obtainLabels(context);
 
-  function checkLabels(labelArray) {
-    return (label) => labelArray.includes(label);
-  }
+   // checks if label is a hard label
+   const isHardLabel = (label) => hardLabels.includes(label);
+   // checks if label is a soft label
+   const isSoftLabel = (label) => softLabels.includes(label);
+   // checks if label is an override label
+   const isOverrideLabel = (label) => overrideSoftLabels.includes(label);
 
-  // function isHardLabel(label) {
-  //   return hardLabels.includes(label);
-  // }
-  // function isSoftLabel(label) {
-  //   return softLabels.includes(label);
-  // }
-  // function isOverrideLabel(label) {
-  //   return overrideSoftLabels.includes(label);
-  // }
+   /** If issue includes hard labels there should be no visual changes - move to the Done column */
+   if (issueLabels.some(isHardLabel)) {
+     return doneColumn;
+   }
 
-  /** if issue includes hard labels there should be no visual changes - move to the Done column */
-  if (issueLabels.some(checkLabels(hardLabels))) {
-    return doneColumn;
-  }
+   /** if issue does not include a hard label, but does contain an override label - move to UAT */
+   if (issueLabels.some(isOverrideLabel)) {
+     return UATColumn;
+   }
 
-  /** if issue does not include a hard label, but does contain an override label - move to UAT */
-  if (issueLabels.some(checkLabels(overrideSoftLabels))) {
-    return UATColumn;
-  }
+   /** if issue includes soft labels (no hard or override) - move to Done */
+   if (issueLabels.some(isSoftLabel)) {
+     return doneColumn;
+   }
 
-  /** if issue includes soft labels (no hard or override) - move to Done */
-  if (issueLabels.some(checkLabels(softLabels))) {
-    return doneColumn;
-  }
-
-  // all other issues go to UAT column
-  return UATColumn;
-}
+   // all other issues go to UAT column
+   return UATColumn;
+ }
 
 module.exports = main;
